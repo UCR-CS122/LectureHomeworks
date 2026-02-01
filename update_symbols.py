34ac3b35f0c12e77805f69a4008e54788f26b6f1
Allan Knight <67027104+ucrdrk@users.noreply.github.com>
2026-02-01 05:39:16 +0000

    Made the Symbol update more dynamic

---
+++ update_symbols.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+section_names = ["end", "size", "start"]
+
+old_sym_name = re.sub(r'[\/\.]', '_', sys.argv[1])
+new_sym_name = re.match(r".*(image\d+).*", sys.argv[2]).group(1)
+
+with open(sys.argv[2], 'w') as file:
+    for sec in section_names:
+        file.write("_binary_" + old_sym_name + "_" + sec + " " + new_sym_name + "_" + sec + "\n")
